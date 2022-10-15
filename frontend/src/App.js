0e7e86520dff8b54721c351adda40405c40e1b49
Justin Welsh <justinwelsh33@gmail.com>
2022-10-14 20:24:20 -0500

    added components and style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,40 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Meme from "./components/Meme";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-
-        <h1 className="text-3xl font-bold underline">
-          Hello tailwind!
-        </h1>
-
-        <div className="card w-96 bg-base-100 shadow-xl my-7">
-          <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes"/></figure>
-          <div className="card-body">
-            <h2 className="card-title">Hello daisyUI</h2>
-            <p>Great success!</p>
-            <div className="card-actions justify-end">
-              <button className="btn btn-primary">Buy Now</button>
-            </div>
-          </div>
-        </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Header />
+        <Meme />
     </div>
   );
 }
