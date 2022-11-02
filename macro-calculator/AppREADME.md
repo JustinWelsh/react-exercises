a3cbdb2b2c3d494089e7d05a58afc2c6e431ca9e
Justin Welsh <justinwelsh33@gmail.com>
2022-11-02 17:38:27 -0500

    update

---
+++ macro-calculator/AppREADME.md
@@ -0,0 +1,4 @@
+# Macro-Calculator
+
+An app that allows users to input the weight of a meat source and get a macro breakdown of the specified weight converted into macros.
+
--- macro-calculator/src/App.js
+++ macro-calculator/src/App.js
@@ -1,18 +1,45 @@
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-            <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
-                </div>
-            </div>
+    const [meatSource, setMeatSource] = useState(null)
+    const [userInput, setUserInput] = useState(null)
+    const [proteinCalc, setProteinCalc] = useState(null)
+
+
+
+    const getProtein = () => meatSource * userInput;
+
+    return (
+    <div className="container mx-auto">
+      <h1 className="text-5xl font-bold text-center p-5">Macro Calculator</h1>
+
+        <div className="form-control w-full max-w-xs mx-auto">
+            <label className="label">
+                <span className="label-text">Choose your meat source</span>
+            </label>
+            <select
+                className="select select-bordered"
+                onChange={(e) => {
+                    setMeatSource(e.target.value)
+                }}
+            >
+                <option disabled selected>Pick one</option>
+                <option value={7.75}>Chicken</option>
+                <option value={5.75}>Fish</option>
+                <option value={7}>Beef</option>
+            </select>
+
+            <label className="label">
+                <span className="label-text">Input ounces</span>
+            </label>
+            <input type="number" placeholder="Type here" className="input input-bordered w-full max-w-xs"
+                   onChange={(e) => {
+                setUserInput(e.target.value)
+            }} />
+            <button type="submit" className="btn my-4" onClick={() => setProteinCalc(meatSource * userInput)}>Get Protein</button>
+
+            {proteinCalc && <div className="bg-white p-3 rounded-md">{proteinCalc} grams</div>}
         </div>
     </div>
   );
