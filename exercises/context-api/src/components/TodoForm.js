31d9713d0cec764d01d84cdbd6bfe43e41aeacd3
Justin Welsh <justinwelsh33@gmail.com>
2023-09-18 22:31:29 -0500

    added small comments

--- exercises/context-api/src/components/TodoForm.js
+++ exercises/context-api/src/components/TodoForm.js
@@ -2,7 +2,9 @@ import React, { useState, useContext } from "react";
 import { TodoContext } from "../context/TodoContext";
 
 function TodoForm() {
+  // Define a local state for the input value
   const [text, setText] = useState("");
+  // TODO: Access the `addTodo` function from the TodoContext using useContext
   const { addTodo } = useContext(TodoContext); //? From context.
 
   const handleSubmit = (e) => {
